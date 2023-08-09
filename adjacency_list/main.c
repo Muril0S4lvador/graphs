5101c8e2dea69968f7e0f7cc6ca17c037246b087
Murilo Salvador <109419353+Muril0S4lvador@users.noreply.github.com>
2023-08-09 01:54:13 -0300

    Update main.c

--- adjacency_list/main.c
+++ adjacency_list/main.c
@@ -6,12 +6,12 @@ int main(){
 
     Graph *graph = graph_construct(5);
 
-    edge_create(graph, 0, adjacency_create(1, 2), UNDIRECTED);
-    edge_create(graph, 1, adjacency_create(2, 4), UNDIRECTED);
-    edge_create(graph, 2, adjacency_create(0, 12), UNDIRECTED);
-    edge_create(graph, 2, adjacency_create(4, 40), UNDIRECTED);
-    edge_create(graph, 3, adjacency_create(1, 3), UNDIRECTED);
-    edge_create(graph, 4, adjacency_create(3, 8), UNDIRECTED);
+    edge_create(graph, 0, adjacency_create(1, 2), DIRECTED);
+    edge_create(graph, 1, adjacency_create(2, 4), DIRECTED);
+    edge_create(graph, 2, adjacency_create(0, 12), DIRECTED);
+    edge_create(graph, 2, adjacency_create(4, 40), DIRECTED);
+    edge_create(graph, 3, adjacency_create(1, 3), DIRECTED);
+    edge_create(graph, 4, adjacency_create(3, 8), DIRECTED);
 
     graph_print(graph);
 
