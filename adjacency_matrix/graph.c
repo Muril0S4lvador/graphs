47cdbac517e853764da9df9f1d7e9d0147927ec7
Murilo Salvador <murilo.salvador95@gmail.com>
2023-08-09 13:15:37 -0300

    Finalizada matrizes de adjacências

---
+++ adjacency_matrix/graph.c
@@ -0,0 +1,46 @@
+#include "graph.h"
+
+Graph *graph_construct(int v){
+    Graph *g = malloc(sizeof(Graph));
+
+    g->num_edges = 0;
+    g->num_vertices = v;
+    g->adj = malloc(v * sizeof(int*));
+
+    for(int i = 0; i < v; i++)
+        g->adj[i] = calloc(v, sizeof(int));
+
+    return g;
+}
+
+void graph_add_edge(Graph *g, int v1, int v2, height peso, int direction){
+    if(direction == UNDIRECTED)
+        g->adj[v2][v1] = peso;
+
+    g->adj[v1][v2] = peso;
+
+    g->num_edges++;
+}
+
+void graph_print(Graph *g){
+    printf("-- ");
+    for(int i = 0; i < g->num_vertices; i++)
+        printf("v%d ", i);
+
+    for(int i = 0; i < g->num_vertices; i++){
+        printf("\n");
+        printf("v%d:", i);
+
+        for(int j = 0; j < g->num_vertices; j++)
+            printf(" %d", g->adj[i][j]);
+
+    }
+    printf("\n");
+}
+
+void graph_destroy(Graph *g){
+    for(int i = 0; i < g->num_vertices; i++)
+        free(g->adj[i]);
+    free(g->adj);
+    free(g);
+}
--- adjacency_matrix/graph.h
+++ adjacency_matrix/graph.h
@@ -1,11 +1,27 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIRECTED 0
+#define UNDIRECTED 1
+
+typedef int** matrix;
+typedef int height;
 
 typedef struct{
-    int size;
-
+    int num_vertices;
+    int num_edges;
+    matrix adj;
 } Graph;
 
+Graph *graph_construct(int v);
+
+void graph_add_edge(Graph *g, int v1, int v2, height peso, int direction);
+
+void graph_print(Graph *g);
+
+void graph_destroy(Graph *g);
 
 #endif
