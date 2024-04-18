b8d9a3494bb3bc876721e6a7f087e5ae36f6ed4a
Murilo Salvador <murilo.salvador95@gmail.com>
2024-04-18 00:07:54 -0300

    Commitar

---
+++ tst.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Especifica o diretório raiz onde as pastas de entrada estão localizadas
+root_folder="entradas"
+
+# Nome do programa a ser executado
+program="./main"
+
+output_file="tabela.txt"
+
+# Cria um arquivo vazio para as saídas consolidadas
+> "$output_file"
+
+# Use o comando "find" para percorrer todos os arquivos em profundidade nas pastas "a," "b," e "e"
+
+# Loop para percorrer as pastas "a," "b," e "e"
+for folder in "$root_folder"/{a,b,e,f}; do
+    # Verifica se a pasta existe
+    if [ "$folder/" ]; then
+        echo "Achado $folder/"
+         for file in $folder/*.vrp; do
+            echo "$file"
+            if [ -f "$file" ]; then
+                echo "$file"
+                echo "Executando $program com $file"
+                $program "$file" >> "$output_file"
+            fi
+        done
+    fi
+done
