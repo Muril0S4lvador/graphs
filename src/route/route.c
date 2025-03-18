c516f88396e9bd2cbc4b66b47aa4a05de5843450
LabES <murilo.dalmonech@edu.ufes.br>
2025-03-18 19:02:15 -0300

    documentação pasta de route

--- src/route/route.c
+++ src/route/route.c
@@ -57,7 +57,6 @@ void route_print(Route *r, int size){
     int sum = 0;
     for(int j = 0; j < size; j++){
 
-        // printf("Route #%d | Demand:%d Cost: %d\n", j + 1, r[j].demand, (int)r[j].cost);
         printf("Route #%d: ", j + 1);
         int size = r[j].size;
         for(int i = 0; i < size; i++){
@@ -74,7 +73,6 @@ void route_print_file(Route *r, int size, FILE *arq){
     int sum = 0;
     for(int j = 0; j < size; j++){
 
-        // printf("Route #%d | Demand:%d Cost: %d\n", j + 1, r[j].demand, (int)r[j].cost);
         fprintf(arq, "Route #%d: ", j + 1);
         int size = r[j].size;
         for(int i = 0; i < size; i++){
@@ -82,7 +80,6 @@ void route_print_file(Route *r, int size, FILE *arq){
         }
         fprintf(arq, "\nDemand: %d Cost: %.0lf\n\n", r[j].demand, r[j].cost);
         sum += r[j].cost;
-        // fprintf(arq, "\n");
     }
     fprintf(arq, "Total Cost: %d\n", sum);
 }
--- src/route/route.h
+++ src/route/route.h
@@ -9,32 +9,46 @@ typedef struct Route Route;
 typedef unsigned char bool;
 typedef int costType;
 
+// Tipagem de impreção do custo da rota
 #define COST_PRINT "%d"
 
+// Constrói uma rota de acordo com o tamanho
 Route* route_construct(int size);
 
+// Retorna a rota de acordo com seu índice
 int *route_return_route(Route *r, int index);
 
+// Retorna o tamanho de uma rota de acordo com seu índice
 int route_return_size(Route *r, int index);
 
+// Retorna a demanda de uma rota de acordo com seu índice
 int route_return_demand(Route *r, int index);
 
+// Retorna o custo de uma rota de acordo com seu índice
 costType route_return_cost(Route *r, int index);
 
+// Retorna o custo total de uma matriz de rotas
 costType route_return_total_cost(Route *r, int size);
 
+// Atribui a ordem de uma rota de acordo com seu índice
 void route_set_route(Route *r, int *route, int index, int size);
 
+// Atribui o tamanho de uma rota de acordo com seu índice
 void route_set_size(Route *r, int size, int index);
 
+// Atribui a demanda de uma rota de acordo com seu índice
 void route_set_demand(Route *r, int demand, int index);
 
+// Atribui o custo de uma rota de acordo com seu índice
 void route_set_cost(Route *r, costType cost, int index);
 
+// Imprime uma rota de acordo com seu índice
 void route_print(Route *r, int index);
 
+// Imprime todas as rotas de uma matriz de rotas r em um arquivo arq
 void route_print_file(Route *r, int size, FILE *arq);
     
+// Libera a memória de todas as rotas de uma matriz de rotas r
 void route_destroy(Route *r, int size);
 
 #endif
