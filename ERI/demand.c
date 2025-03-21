e01fbdd8c62203af281f5db0ab9009aa36123d87
Murilo <murilo.salvador95@gmail.com>
2025-03-21 16:17:16 -0300

    feat: função de criar instancia a partir de cb

--- ERI/demand.c
+++ ERI/demand.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <math.h>
 
+#define DEN 7.5
+#define APERTO 0.92
+
+char nome[100] = "";
+char comment[100] = "";
+
+typedef struct {
+    int id, demand;
+    double x, y;
+} Node;
+
+
+void write(Node *instance, int dimension, float totalDemand, FILE *f);
+
 /**
  * Recebe uma instância de um algoritmo de cobertura de vértices
  * e ajusta os parâmetros para que fique mais parecido com as
@@ -14,36 +28,89 @@ int main ( int argc, char* argv[] ) {
     FILE *arq = fopen(argv[1], "r");
     if(!arq) exit(printf("Eror fopen failed.\n"));
 
-    char buffer[100] = "hi";
-    while (strcmp(buffer, "DEMEND_SECTION\n"))
+    char buffer[1000];
+    int dimension = 0, capacity = 0;
+    fgets(buffer, sizeof(buffer), arq); // NAME
+    char name[50];
+    sscanf(buffer, "%*[^:]: %s", name);
+    fgets(buffer, sizeof(buffer), arq); // COMMENT
+    char *token = strtok(buffer, "()"); token = strtok(NULL, ")");
+    token[strlen(token) - 1] = (token[strlen(token) - 1] == '\n') ? '\0' : token[strlen(token) - 1]; // tira \n caso precise
+    strcpy(comment, token);
+    fgets(buffer, sizeof(buffer), arq); // TYPE
+    fgets(buffer, sizeof(buffer), arq); // DIMENSION
+    sscanf(buffer, "%*[^:]: %d", &dimension);
+    
+    Node *instance = malloc(sizeof(Node) * dimension);
+    double m[dimension][3];
+
+    while (strcmp(buffer, "NODE_COORD_SECTION\n")) // NODE_COORD_SECTION
     {
         fgets(buffer, sizeof(buffer), arq);
     }
 
-    fgets(buffer, sizeof(buffer), arq);
-    int sum = 0, qtd = 0;
-    while(strcmp(buffer, "EDGE_WEIGHT_SECTION\n")){
-        int demand = 0;
-        sscanf(buffer, "%*d %d", &demand);
-        sum += demand;
-        qtd++;
+    for(int i = 0; i < dimension; i++){
         fgets(buffer, sizeof(buffer), arq);
+        sscanf(buffer, "%*d %lf %lf", &m[i][0], &m[i][1]);
     }
+    fgets(buffer, sizeof(buffer), arq); // DEMEND_SECTION
+    int totalDemand = 0;
+    for(int i = 0; i < dimension; i++){
+        
+        fgets(buffer, sizeof(buffer), arq); 
+        sscanf(buffer, "%*d %lf", &m[i][2]);
+        
+        instance[i].x = m[i][0];
+        instance[i].y = m[i][1];
+        instance[i].demand = m[i][2];
 
-    printf("Demanda total: %d\nNós: %d\n\n", sum, qtd);
+        totalDemand += m[i][2];
+    }
+    
+    fgets(buffer, sizeof(buffer), arq); // EDGE_WEIGHT_SECTION
 
-    // Capacidade total dos caminhões
-    int capTrucks = ceil( sum / 0.92 );
-    // Quantidade de caminhões
-    int numTrucks = ceil( qtd / 7.5 );
-    // Capacidade individual de cada caminhão
-    int capacity = ceil( capTrucks / numTrucks );
-
-    printf("Full capacity: %d\n", capTrucks);
-    printf("Trucks: %d\n", numTrucks);
-    printf("Capacity: %d\n", capacity);
+    write(instance, dimension, totalDemand, arq);
 
     return 0;
+}
+
+void write(Node *instance, int dimension, float totalDemand, FILE *f) {
+    int qtdCaminhao = ceil(dimension / DEN);
+    int caminhaoCap = ceil(totalDemand / APERTO);
+    int demanda = ceil(caminhaoCap / qtdCaminhao);
+
+    sprintf(nome, "C-n%d-k%d.vrp", dimension, qtdCaminhao);
+
+    FILE *arq = fopen(nome, "w");
+
+    fprintf(arq, "NAME : %s\n", nome);
+    fprintf(arq, "COMMENT : (%s)\n", comment);
+    fprintf(arq, "TYPE : CVRP\n");
+    fprintf(arq, "DIMENSION : %d\n", dimension);
+    fprintf(arq, "DIMENSION : LOWER_ROW_VIX\n");
+    fprintf(arq, "CAPACITY : %d\n", demanda);
+    fprintf(arq, "NODE_COORD_SECTION\n");
+    
+    for(int i = 0; i < dimension; i++) {
+        fprintf(arq, "%d %.3lf %.3lf\n", i + 1, instance[i].x, instance[i].y);
+    }
+    
+    fprintf(arq, "DEMAND_SECTION\n");
+    
+    for(int i = 0; i < dimension; i++) {
+        fprintf(arq, "%d %d\n", i + 1, instance[i].demand);
+    }
+    fprintf(arq, "EDGE_WEIGHT_SECTION\n");
+
+    char buffer[1000] = "";
+    while (!feof(f))
+    {
+        fgets(buffer, sizeof(buffer),f);
+        fputs(buffer, arq);
+    }
+    
+    fclose(arq);
+    fclose(f);
 }
 
 /*
